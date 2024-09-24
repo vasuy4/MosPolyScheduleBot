e28cee265ab680eb4b2c2dd07109e6ed040f08ab
Ryzhov Artem <ar.ryqp@mail.ru>
2024-09-24 22:04:11 +0300

    format with black

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -5,5 +5,8 @@ from loader import bot
 
 @bot.message_handler(commands=["start"])
 def bot_start(message: Message):
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}! Я бот, который может подсказать тебе по расписанию "
-                          f"Московского Политеха. Введите /help для просмотра функционала.")
+    bot.reply_to(
+        message,
+        f"Привет, {message.from_user.full_name}! Я бот, который может подсказать тебе по расписанию "
+        f"Московского Политеха. Введите /help для просмотра функционала.",
+    )
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -3,6 +3,4 @@ from config_data.config import DEFAULT_COMMANDS
 
 
 def set_default_commands(bot):
-    bot.set_my_commands(
-        [BotCommand(*i) for i in DEFAULT_COMMANDS]
-    )
+    bot.set_my_commands([BotCommand(*i) for i in DEFAULT_COMMANDS])
